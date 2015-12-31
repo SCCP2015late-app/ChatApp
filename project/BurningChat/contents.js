d2f02d6aaa32651dbc9ce728a12dc4b0bd5ea338
misoton665 <misoton998@hotmail.co.jp>
2015-12-31 10:53:27 +0900

    Fix "let" to "const" in contents.js

--- project/BurningChat/contents.js
+++ project/BurningChat/contents.js
@@ -1,33 +1,33 @@
 "use strict";
 // Code for Debug[START]
-let GROUP_NAME = "mogemoge group";
-let GROUP_ID = 1919;
+const GROUP_NAME = "mogemoge group";
+const GROUP_ID = 1919;
 
-let NAME_00 = "owner";
-let NAME_01 = "poe";
-let NAME_02 = "maguro";
-let NAME_03 = "fupiyon";
+const NAME_00 = "owner";
+const NAME_01 = "poe";
+const NAME_02 = "maguro";
+const NAME_03 = "fupiyon";
 
-let EMAIL_00 = "";
-let EMAIL_01 = "";
-let EMAIL_02 = "";
-let EMAIL_03 = "";
+const EMAIL_00 = "";
+const EMAIL_01 = "";
+const EMAIL_02 = "";
+const EMAIL_03 = "";
 
-let PROP_00 = new RegistrationItem(NAME_00, EMAIL_00);
-let PROP_01 = new RegistrationItem(NAME_01, EMAIL_01);
-let PROP_02 = new RegistrationItem(NAME_02, EMAIL_02);
-let PROP_03 = new RegistrationItem(NAME_03, EMAIL_03);
+const PROP_00 = new RegistrationItem(NAME_00, EMAIL_00);
+const PROP_01 = new RegistrationItem(NAME_01, EMAIL_01);
+const PROP_02 = new RegistrationItem(NAME_02, EMAIL_02);
+const PROP_03 = new RegistrationItem(NAME_03, EMAIL_03);
 
-let OWNER = new Owner(0, PROP_00);
+const OWNER = new Owner(0, PROP_00);
 
-let MEMBERS = [
+const MEMBERS = [
     OWNER,
     new Participant(1, PROP_01),
     new Participant(2, PROP_02),
     new Participant(3, PROP_03)
   ];
   
-let YOU = new Participant(2, new RegistrationItem("magro", ""));
+const YOU = new Participant(2, new RegistrationItem("magro", ""));
 // Code for Debug[END]
 
 var youOrNot = function(user) {
