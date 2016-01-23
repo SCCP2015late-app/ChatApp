78db2ee7357eceb242b8930d29c94c8fc1cdbade
RomTin <remio_snow_nosound_bythewindow@yahoo.co.jp>
2016-01-23 23:00:45 +0900

    clean and divide source codes

--- project/BurningChat/bgmodules.js
+++ project/BurningChat/bgmodules.js
@@ -8,9 +8,10 @@ var hash = CryptoJS.MD5("Message");
 */
 //get internal IP address
 
-const msg_port = "22222";
-const pict_port = "33333";
-const req_port = "44444";
+const LOCALHOST = '127.0.0.1';
+const msg_port = 22222;
+const req_port = 33333;
+const sample_message = "This is a test message.";
 
 var getLocalIP = function(callback) {
     var ips = [];
@@ -40,30 +41,10 @@ var getLocalIP = function(callback) {
     });
 };
 
-//ArrayBuffer String converetr
-var string_to_buffer = function(src) {
-    return (new Uint16Array([].map.call(src, function(c) {
-        return c.charCodeAt(0);
-    }))).buffer;
-};
-
-var buffer_to_string = function(buf) {
-    return String.fromCharCode.apply("", new Uint16Array(buf));
-};
-
-
-//test code for udp packet sending and receiving
-var bind_address = '127.0.0.1';
-var bind_port = 22222;
-var add = bind_address/* = getLocalIP (function(ips) { return ips[ips.length - 1]; })*/;
-var m = "This is a test message.";
-
 chrome.system.network.getNetworkInterfaces(function(ipinfo){
     var add = [];
     var name = [];
     for(var i in ipinfo){
-//        name.push(ipinfo[ipinfo.length - 1].name);
-//        add.push(ipinfo[ipinfo.length - 1].address);
         console.log(ipinfo[i].name + " " + ipinfo[i].address);
     }
 });
@@ -72,17 +53,15 @@ var receiveCallback = function(info){
     console.log(info.socketId + " : " + buffer_to_string(info.data));
 };
 
-var data = string_to_buffer(m);
 chrome.sockets.udp.create({}, function(createInfo) {
     chrome.sockets.udp.onReceive.addListener(receiveCallback);
-	chrome.sockets.udp.bind(createInfo.socketId, add, bind_port, function(result){
-		chrome.sockets.udp.send(createInfo.socketId, data, add/*bind_address*/, bind_port, function(sendInfo) {
-			console.log('poe: ' + sendInfo.resultCode);
+	chrome.sockets.udp.bind(createInfo.socketId, LOCALHOST, msg_port, function(result){
+		chrome.sockets.udp.send(createInfo.socketId, string_to_buffer(sample_message), LOCALHOST, msg_port, function(sendInfo) {
+			console.log('sent done: ' + sendInfo.resultCode);
 			chrome.sockets.udp.close(createInfo.socketId, function(){});
 		});
 	});
 });
-//end---
 
 
 /* global onUpdateMessageListener */
@@ -109,13 +88,6 @@ function msgBroadcastRequest(message){//massageを受け取ってjsonにしてow
 
 }
 
-//TODO least priority
-/*function pictBroadcastRequest(pict){
-    pict["data"] = base64encode();//バイナリを読みこませる
-    var json_pict = JSON.stringify(pict);
-    pictBroadcast(json_pict);
-}*/
-
 function msgObjectRecv(){
     if(obj.isMessage == true){
         storeMessage(obj);
@@ -124,23 +96,10 @@ function msgObjectRecv(){
     }
 }
 
-//TODO least priority
-/*function pictObjectRecv(){
-    if(obj.isPicture == true){
-        savePict(obj);
-    }
-}*/
-
 function updateMsgList(msg){
     var message = msg["body"];
     Env().onUpdateMessageListener.addCallback(message);
 }
-//TODO least priority
-/*function savePict(filename, encodedPict){
-    chrome.storage.sync.set({'filename':base64decode(encodedPict)},function(){
-        console.log("This is callback");
-    });
-}*/
 
 function sendGroupInfo(){
     var group_info = jsonizeGroupInfo();
@@ -215,61 +174,4 @@ function jsonizeGroupInfo(){
         "msg_list": messages
     }, null, "    ");
    return g_info;
-}
-
-// BASE64 (RFC2045) Encode/Decode for string in JavaScript
-// Version 1.2 Apr. 8 2004 written by MIZUTANI Tociyuki
-// Copyright 2003-2004 MIZUTANI Tociyuki
-//
-// This code is free software; you can redistribute it and/or
-// modify it under the terms of the GNU Library General Public
-// License as published by the Free Software Foundation; either
-// version 2 of the License, or (at your option) any later version.
-//
-// usage:
-// base64 = base64encode(string)  Encode a string.
-// string = base64decode(base64)  Decode a base64 string.
-//
-// caution:
-// 1) Wide characters like japanese kanji are not supported. Use only in Latin-1.
-
-var base64list = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
-
-function base64encode(s) {
-    var t = '', p = -6, a = 0, i = 0, v = 0, c;
-
-    while ( (i < s.length) || (p > -6) ) {
-        if ( p < 0 ) {
-            if ( i < s.length ) {
-                c = s.charCodeAt(i++);
-                v += 8;
-            } else {
-                c = 0;
-            }
-            a = ((a&255)<<8)|(c&255);
-            p += 8;
-        }
-        t += base64list.charAt( ( v > 0 )? (a>>p)&63 : 64 )
-            p -= 6;
-        v -= 6;
-    }
-    return t;
-}
-
-function base64decode(s) {
-    var t = '', p = -8, a = 0, c, d;
-
-    for( var i = 0; i < s.length; i++ ) {
-        if ( ( c = base64list.indexOf(s.charAt(i)) ) < 0 )
-            continue;
-        a = (a<<6)|(c&63);
-        if ( ( p += 6 ) >= 0 ) {
-            d = (a>>p)&255;
-            if ( c != 64 )
-                t += String.fromCharCode(d);
-            a &= 63;
-            p -= 8;
-        }
-    }
-    return t;
 }
